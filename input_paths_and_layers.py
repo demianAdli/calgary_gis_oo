5d75c6e2e102c226f9ec9ceae0f6a6e30c9ef91d
Alireza Adli <alireza.adli@mail.concordia.ca>
2025-10-16 12:34:27 -0400

    Define paths

--- input_paths_and_layers.py
+++ input_paths_and_layers.py
@@ -21,37 +21,20 @@ qgis_path = 'C:/Program Files/QGIS 3.34.1/apps/qgis'
 
 # Gathering input data layers paths
 input_paths = {
-  'NRCan':
-  'C:/Users/a_adli/PycharmProjects/mtl_gis_oo/input_data/nrcan/'
-  'Autobuilding_QC_VILLE_MONTREAL.shp',
-  'GeoIndex':
-  'C:/Users/a_adli/PycharmProjects/mtl_gis_oo/input_data/'
-  'Geoindex_81670/mamh_usage_predo_2022_s_poly.shp',
-  'Property Assessment':
-  'C:/Users/a_adli/PycharmProjects/mtl_gis_oo/input_data/'
-  'property_assessment/uniteevaluationfonciere.shp',
-  'CERC Boundary':
-  'C:/Users/a_adli/PycharmProjects/mtl_gis_oo/input_data/'
-  'cerc_district_111/layer_111.shp'
+  'Property Assessment Calgary':
+  'C:/Users/a_adli/docker_projects/calgary_gis_oo/input_data/'
+  'property_assessment_calgary/property_assessment_calgary.shp',
+  'Calgary 209':
+  'C:/Users/a_adli/docker_projects/calgary_gis_oo/input_data/'
+  'property_assessment_calgary_district_209/'
+  'property_assessment_calgary_district_209.shp'
 }
 
 # Defining a directory for all the output data layers
-output_paths_dir = \
-  'C:/Users/a_adli/PycharmProjects/mtl_gis_oo/' \
-  'output_data'
+output_paths_dir = 'C:/Users/a_adli/docker_projects/calgary_gis_oo/output_data'
 
 # Preparing a bedding for output data layers paths
 output_paths = {
-  'Fixed NRCan': '',
-  'NRCan CERC Fixed': '',
-  'Fixed GeoIndex': '',
-  'Clipped Fixed GeoIndex': '',
-  'CERC Property Assessment': '',
-  'Splitted CERC NRCans': '',
-  'Pairwise Clipped Property Assessment Partitions': '',
-  'Pairwise Clipped Merged Property Assessment': '',
-  'Property Assessment and NRCan': '',
-  'Property Assessment and NRCan and GeoIndex': '',
-  'Deleted Duplicates Layer': '',
-  'Single Parts Layer': ''
+  'Fixed Property Assessment Calgary': '',
+  'Fixed Calgary 209': '',
 }
